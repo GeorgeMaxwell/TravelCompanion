25a6f80a9ec1f1af9d7e6de5eea1b6f15183da09
cs17bjm <1624079@brunel.ac.uk>
2018-02-17 17:37:58 +0000

    fixed issue with address search box

--- Main/js/main.js
+++
@@ -1,5 +0,0 @@
-$( document ).ready(function() {
-    var input = document.getElementById('pac-input');
-            var searchBox = new google.maps.places.SearchBox(input);
-
-});
---
+++ Main/static/js/main.js
@@ -0,0 +1,7 @@
+$( document ).ready(function() {
+    var work = document.getElementById('pac-work');
+            var workSearch = new google.maps.places.SearchBox(work);
+    var home = document.getElementById('pac-home');
+            var homeSearch = new google.maps.places.SearchBox(home);
+
+});

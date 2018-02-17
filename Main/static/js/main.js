e3aad54447a0f7e1e7a8821fde98fe0f682a2b86
Diogo Costa <31896387+DiogoCosta1998@users.noreply.github.com>
2018-02-17 19:40:56 +0000

    Added onclick event for update button

--- Main/static/js/main.js
+++ Main/static/js/main.js
@@ -1,7 +1,12 @@
 $( document ).ready(function() {
     var work = document.getElementById('pac-work');
-            var workSearch = new google.maps.places.SearchBox(work);
+    var workSearch = new google.maps.places.SearchBox(work);
+
     var home = document.getElementById('pac-home');
-            var homeSearch = new google.maps.places.SearchBox(home);
+    var homeSearch = new google.maps.places.SearchBox(home);
+
+    $("#updateBtn").click(function(){
+        alert("Your details have been updated!");
+    });
 
 });

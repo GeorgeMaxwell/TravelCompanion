1a436547cd3868e1ef4c28f98072dd147f510486
Diogo Costa <31896387+DiogoCosta1998@users.noreply.github.com>
2018-02-17 20:55:50 +0000

    Now able to get latitude and longitude
    Latitude and longitude now being retrieved from the home address

--- Main/static/js/main.js
+++ Main/static/js/main.js
@@ -7,6 +7,28 @@ $( document ).ready(function() {
 
     $("#updateBtn").click(function(){
         alert("Your details have been updated!");
+        var geocoder = new google.maps.Geocoder();
+        geocodeAddress(geocoder);
+        //console.log(homeSearch.getBounds());
     });
+
+    function geocodeAddress(geocoder) {
+        var address = $('#pac-home').val();
+        alert(address)
+        geocoder.geocode( { 'address': address}, function(results, status) {
+
+        if (status == google.maps.GeocoderStatus.OK) {
+            console.log(results[0].geometry.location.lat())
+            console.log(results[0].geometry.location.lng())
+            //var latitude = results[0].geometry.location.latitude;
+            //var longitude = results[0].geometry.location.longitude;
+            //alert(latitude);
+            }
+
+        else {
+            alert("ERROR!")
+        }
+    });
+    }
 
 });

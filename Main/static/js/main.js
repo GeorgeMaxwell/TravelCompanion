0d5ba11d9ae247641a296daa25db43677f74fb55
cs17bjm <1624079@brunel.ac.uk>
2018-02-17 22:08:50 +0000

    added return for coordinates

--- Main/static/js/main.js
+++ Main/static/js/main.js
@@ -5,30 +5,36 @@ $( document ).ready(function() {
     var home = document.getElementById('pac-home');
     var homeSearch = new google.maps.places.SearchBox(home);
 
+        
     $("#updateBtn").click(function(){
         alert("Your details have been updated!");
         var geocoder = new google.maps.Geocoder();
-        geocodeAddress(geocoder);
+        //var ans = geocodeAddress(geocoder,$('#pac-home').val());
+        geocodeAddress(geocoder,$('#pac-home').val(), function(search_latlng) {
+          console.log(search_latlng);
+
+        });
+       geocodeAddress(geocoder,$('#pac-work').val(), function(search_latlng) {
+            console.log(search_latlng);
+
+        });
+        //console.log(ans);
         //console.log(homeSearch.getBounds());
     });
 
-    function geocodeAddress(geocoder) {
-        var address = $('#pac-home').val();
-        alert(address)
+    function geocodeAddress(geocoder,address, callback) {
+
+        var latlng = new Array(2);
+
         geocoder.geocode( { 'address': address}, function(results, status) {
-
-        if (status == google.maps.GeocoderStatus.OK) {
-            console.log(results[0].geometry.location.lat())
-            console.log(results[0].geometry.location.lng())
-            //var latitude = results[0].geometry.location.latitude;
-            //var longitude = results[0].geometry.location.longitude;
-            //alert(latitude);
+            if (status == google.maps.GeocoderStatus.OK) {
+                latlng[0] = results[0].geometry.location.lat();
+                latlng[1] = results[0].geometry.location.lng();
+                callback(latlng); // call the callback function here
+            } else {
+                console.log("Geocode was not successful for the following reason: " + status);
             }
-
-        else {
-            alert("ERROR!")
-        }
-    });
+        });
     }
 
 });
